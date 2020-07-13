5e8d73db3c8925b4b5eac797d3945a8bdb9f5c13
PondiB <pondibrian@gmail.com>
2020-07-13 19:09:22 +0300

    Polymorphism using cars analogy

---
+++ 007-OOP-Part-2-Composition-Encapsulation-and-Polymorphism/Polymorphism/src/com/brianpondi/Main.java
@@ -0,0 +1,130 @@
+package com.brianpondi;
+
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+}
+
+class Audi extends Car {
+
+    public Audi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Audi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Audi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Audi -> brake()";
+    }
+}
+
+class Mercedes extends Car {
+
+    public Mercedes(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() +  " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() +  " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() +  " -> brake()";
+    }
+}
+
+class Ford extends Car {
+
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Polymorphism
+
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Car audi = new Audi(6, "2020 S5 Sport Back");
+        System.out.println(audi.startEngine());
+        System.out.println(audi.accelerate());
+        System.out.println(audi.brake());
+
+        Car ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Mercedes mercedes = new Mercedes(4, "Class C 200");
+        System.out.println(mercedes.startEngine());
+        System.out.println(mercedes.accelerate());
+        System.out.println(mercedes.brake());
+    }
+
+
+}
