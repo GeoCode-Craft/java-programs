ca621283bde32e84b8746880bdf6a9492495ccc2
PondiB <pondibrian@gmail.com>
2020-07-23 23:55:18 +0300

    Inner classes resolved

--- 009-Inner-and-Abstract-Classes-&-Interfaces/InnerClassesChallenge/src/com/brianpondi/Main.java
+++ 009-Inner-and-Abstract-Classes-&-Interfaces/InnerClassesChallenge/src/com/brianpondi/Main.java
@@ -1,88 +1,204 @@
 package com.brianpondi;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    private static ArrayList<Album> albums = new ArrayList<Album>();
+
     public static void main(String[] args) {
-        // Create a simple interface (tip: name it Saveable or ISaveable)
-// That interface allows an object to be saved to some sort of storage medium. (tip: use arraylist)
-// The exact type of medium is not known to the interface (nor to the classes that implement it).
-// The interface will just specify two methods, one to return an ArrayList of values to be saved
-// and the other to populate the object's fields from an ArrayList (parameter).
-//
-// Create a few sample classes that implement your Saveable interface (we've used the idea of a game,
-// with Players and Monsters, but you can create any type of classes that you want).
-//
-// Override the toString() method for each of your classes so that they can be easily printed to enable
-// the program to be tested easier.
-//
-// In Main, write a method that takes an object that implements the interface as a parameter and
-// "saves" the values e.g. calls the method defined in the interface.
+	    // Create a program that implements a playlist for songs
+        // Create a Song class having Title and Duration for a song.
+        // The program will have an Album class containing a list of songs.
+        // The albums will be stored in an ArrayList
+        // Songs from different albums can be added to the playlist and will appear in the list in the order
+        // they are added.
+        // Once the songs have been added to the playlist, create a menu of options to:-
+        // Quit,Skip forward to the next song, skip backwards to a previous song.  Replay the current song.
+        // List the songs in the playlist
+        // A song must exist in an album before it can be added to the playlist (so you can only play songs that
+        // you own).
+        // Hint:  To replay a song, consider what happened when we went back and forth from a city before we
+        // started tracking the direction we were going.
+        // As an optional extra, provide an option to remove the current song from the playlist
+        // (hint: listiterator.remove()
 
-// We haven't covered I/O yet, so your method should just print the values to the screen.
-// Also in the Main class, write a method that restores the values to a Saveable object
-// e.g. calls the method from the interface for populating fields (see above).
+        Album album = new Album("Stormbringer", "Deep Purple");
+        album.addSong("Stormbringer", 4.6);
+        album.addSong("Love don't mean a thing", 4.22);
+        album.addSong("Holy man", 4.3);
+        album.addSong("Hold on", 5.6);
+        album.addSong("Lady double dealer", 3.21);
+        album.addSong("You can't do it right", 6.23);
+        album.addSong("High ball shooter", 4.27);
+        album.addSong("The gypsy", 4.2);
+        album.addSong("Soldier of fortune", 3.13);
+        albums.add(album);
 
-// Again, we are not going to use Java file I/O; instead use the readValues() method below to
-// simulate getting values from a file – this allows you to type as many values as your class
-// requires, and returns an ArrayList.
-// There is a whole Java I/O section later in the course where you will get to use files, etc.
-        Player brian = new Player("Brian", 10, 15);
-        System.out.println(brian.toString());
-        saveObject(brian);
+        album = new Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        album.addSong("I put the finger on you", 3.25);
+        album.addSong("Lets go", 3.45);
+        album.addSong("Inject the venom", 3.33);
+        album.addSong("Snowballed", 4.51);
+        album.addSong("Evil walks", 3.45);
+        album.addSong("C.O.D.", 5.25);
+        album.addSong("Breaking the rules", 5.32);
+        album.addSong("Night of the long knives", 5.12);
+        albums.add(album);
 
-        brian.setHitPoints(8);
-        System.out.println(brian);
-        brian.setWeapon("Axe");
-        saveObject(brian);
-        //loadObject(brian);
-        System.out.println(brian);
+        LinkedList<Song> playList = new LinkedList<Song>();
+        albums.get(0).addToPlayList("You can't do it right", playList);
+        albums.get(0).addToPlayList("Holy man", playList);
+        albums.get(0).addToPlayList("Speed king", playList);  // Does not exist
+        albums.get(0).addToPlayList(9, playList);
+        albums.get(1).addToPlayList(8, playList);
+        albums.get(1).addToPlayList(3, playList);
+        albums.get(1).addToPlayList(2, playList);
+        albums.get(1).addToPlayList(24, playList);  // There is no track 24
 
-        ISaveable werewolf = new Monster("Werewolf", 20, 45);
-        System.out.println(((Monster) werewolf).getStrength());
-        saveObject(werewolf);
+        play(playList);
+
+
+
+
     }
 
-    public static ArrayList<String> readValues() {
-        ArrayList<String> values = new ArrayList<String>();
-
+    private static void play(LinkedList<Song> playList) {
         Scanner scanner = new Scanner(System.in);
         boolean quit = false;
-        int index = 0;
-        System.out.println("Choose\n" +
-                "1 to enter a string\n" +
-                "0 to quit");
+        boolean forward = true;
+        ListIterator<Song> listIterator = playList.listIterator();
+        if(playList.size() == 0) {
+            System.out.println("No songs in playlist");
+            return;
+        } else {
+            System.out.println("Now playing " + listIterator.next().toString());
+            printMenu();
+        }
 
-        while (!quit) {
-            System.out.print("Choose an option: ");
-            int choice = scanner.nextInt();
+        while(!quit) {
+            int action = scanner.nextInt();
             scanner.nextLine();
-            switch (choice) {
+
+            switch(action) {
                 case 0:
+                    System.out.println("Playlist complete.");
                     quit = true;
                     break;
                 case 1:
-                    System.out.print("Enter a string: ");
-                    String stringInput = scanner.nextLine();
-                    values.add(index, stringInput);
-                    index++;
+                    if(!forward) {
+                        if(listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+                    if(listIterator.hasNext()) {
+                        System.out.println("Now playing " + listIterator.next().toString());
+                    } else {
+                        System.out.println("We have reached the end of the playlist");
+                        forward = false;
+                    }
                     break;
+
+                case 2:
+                    if(forward) {
+                        if(listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        forward = false;
+                    }
+                    if(listIterator.hasPrevious()) {
+                        System.out.println("Now playing " + listIterator.previous().toString());
+                    } else {
+                        System.out.println("We are at the start of the playlist");
+                        forward = true;
+                    }
+                    break;
+                case 3:
+                    if(forward) {
+                        if(listIterator.hasPrevious()) {
+                            System.out.println("Now replaying " + listIterator.previous().toString());
+                            forward = false;
+                        } else {
+                            System.out.println("We are at the start of the list");
+                        }
+                    } else {
+                        if(listIterator.hasNext()) {
+                            System.out.println("Now replaying " + listIterator.next().toString());
+                            forward = true;
+                        } else {
+                            System.out.println("We have reached the end of the list");
+                        }
+                    }
+                    break;
+                case 4:
+                    printList(playList);
+                    break;
+                case 5:
+                    printMenu();
+                    break;
+
+                case 6:
+                    if(playList.size() >0) {
+                        listIterator.remove();
+                        if(listIterator.hasNext()) {
+                            System.out.println("Now playing " + listIterator.next());
+                        } else if(listIterator.hasPrevious()) {
+                            System.out.println("Now playing " + listIterator.previous());
+                        }
+                    }
+                    break;
+
             }
         }
-        return values;
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions:\npress");
+        System.out.println("0 - to quit\n" +
+                "1 - to play next song\n" +
+                "2 - to play previous song\n" +
+                "3 - to replay the current song\n" +
+                "4 - list songs in the playlist\n" +
+                "5 - print available actions.\n" +
+                "6 - delete current song from playlist");
 
     }
 
-    public static void saveObject(ISaveable objectToSave){
-        for(int i = 0; i<objectToSave.write().size(); i++){
-            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+
+    private static void printList(LinkedList<Song> playList) {
+        Iterator<Song> iterator = playList.iterator();
+        System.out.println("================================");
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
+        System.out.println("================================");
     }
 
-    public static void loadObject(ISaveable objectToLoad){
-        ArrayList<String> values = readValues();
-        objectToLoad.read(values);
-    }
+
+
+    // Modify the playlist challenge so that the Album class uses an inner class.
+    // Instead of using an ArrayList to hold its tracks, it will use an inner class called SongList
+    // The inner SongList class will use an ArrayList and will provide a method to add a song.
+    // It will also provide findSong() methods which will be used by the containing Album class
+    // to add songs to the playlist.
+    // Neither the Song class or the Main class should be changed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
