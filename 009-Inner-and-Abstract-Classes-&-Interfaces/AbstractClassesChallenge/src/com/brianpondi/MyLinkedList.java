eff8e54e71457d6808f3403a2d6841d4c351318a
PondiB <pondibrian@gmail.com>
2020-07-27 00:14:36 +0300

    bugs in myLinkedList remove method resolved

--- 009-Inner-and-Abstract-Classes-&-Interfaces/AbstractClassesChallenge/src/com/brianpondi/MyLinkedList.java
+++ 009-Inner-and-Abstract-Classes-&-Interfaces/AbstractClassesChallenge/src/com/brianpondi/MyLinkedList.java
@@ -46,7 +46,7 @@ public class MyLinkedList implements NodeList {
                     this.root = newItem;
                 }
                 return true;
-            }else{
+            } else {
                 // equal
                 System.out.println(newItem.getValue() + " is already present, not added.");
                 return false;
@@ -58,44 +58,51 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public boolean removeItem(ListItem item) {
-        if(item != null){
-            System.out.println("Deleting item "+ item.getValue());
+        if(item != null) {
+            System.out.println("Deleting item " + item.getValue());
         }
+
         ListItem currentItem = this.root;
-        while(currentItem != null){
+        while(currentItem != null) {
             int comparison = currentItem.compareTo(item);
-            //found the item to delete
-            if(currentItem ==this.root){
-                this.root = currentItem.next();
-            }else{
-                currentItem.previous().setNext(currentItem.next());
-                if(currentItem != null){
-                    currentItem.next().setPrevious(currentItem.previous());
+            if(comparison == 0) {
+                // found the item to delete
+                if(currentItem == this.root) {
+                    this.root = currentItem.next();
+                } else {
+                    currentItem.previous().setNext(currentItem.next());
+                    if(currentItem.next() != null) {
+                        currentItem.next().setPrevious(currentItem.previous());
+                    }
                 }
                 return true;
-            }else if (comparison<0) {
+            } else if(comparison <0) {
                 currentItem = currentItem.next();
-            }else{
+            } else { // comparison > 0
+                // We are at an item greater than the one to be deleted
+                // so the item is not in the list
                 return false;
             }
-            return false;
-
-        }
-    }
-
-    @Override
-    public void traverse(ListItem root) {
-        if(root == null){
-            System.out.println("The list is empty");
-        }else {
-            while (root != null) {
-                System.out.println(root.getValue());
-                root = root.next();
-            }
         }
 
+        // We have reached the end of the list
+        // Without finding the item to delete
+        return false;
     }
 
 
+@Override
+public void traverse(ListItem root){
+        if(root==null){
+        System.out.println("The list is empty");
+        }else{
+        while(root!=null){
+        System.out.println(root.getValue());
+        root=root.next();
+        }
+        }
 
-}
+        }
+
+
+        }
